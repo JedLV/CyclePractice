ccdf23facce544f0fa388c0ce751d2db6aa6e57f
编程 <1660873558@qq.com>
2021-07-12 17:07:12 +0800

    用for循环打印关于1的九九乘法表

---
+++ src/ForTest5.java
@@ -0,0 +1,11 @@
+/*
+用for循环打印关于1的九九乘法表
+ */
+
+public class ForTest5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++){
+            System.out.println("1 * " + i + " = " + (1 * i) + "");
+        }
+    }
+}
