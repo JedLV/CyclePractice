0d07725ef6773054d3e6a241c469bd8de655acfc
编程 <1660873558@qq.com>
2021-07-12 17:58:58 +0800

    用while循环打印100以内的所有奇数和

---
+++ src/WhileTest2.java
@@ -0,0 +1,18 @@
+/*
+用while循环打印100以内的所有奇数和
+ */
+
+public class WhileTest2 {
+    public static void main(String[] args) {
+        int i = 1; int sum = 1;
+        while (i < 100){
+            i++;
+            if (i % 2 == 1){
+                sum = sum + i;
+                System.out.println(i);
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
