e59a3d3d121634df053391a0dfab8e69ff39ae9e
编程 <1660873558@qq.com>
2021-07-12 16:23:59 +0800

    Initial commit

---
+++ src/ForTest1.java
@@ -0,0 +1,12 @@
+/*
+用for循环打印100以内的所有整数
+ */
+public class ForTest1 {
+    public static void main(String[] args) {
+       for (int i = 1 ; i <= 100; i++){
+           System.out.println(i);
+
+       }
+
+    }
+}
